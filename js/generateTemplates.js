ab368c7af1197bb6792122280657126e74e02e18
alucard <knyazev.activ@yandex.ru>
2021-06-24 19:55:00 +0300

    Добавляет интерактивную карту и дополняет реализацию переключения активного/неактивного состояния страницы

--- js/generateTemplates.js
+++ js/generateTemplates.js
@@ -1,4 +1,3 @@
-const mapCanvas = document.querySelector('.map__canvas');
 const similarCardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const idToNameMap = {
   flat: 'Квартира',
@@ -23,43 +22,41 @@ const removeChildrens = ((parent) => {
   }
 });
 
-const generateCards = ((data) => {
-  data.forEach((card) => {
-    const cardElement = similarCardTemplate.cloneNode(true);
-    cardElement.querySelector('.popup__title').textContent = card.offer.title;
-    cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
-    cardElement.querySelector('.popup__text--price').textContent = `${card.offer.price} ₽/ночь`;
-    cardElement.querySelector('.popup__type').textContent = getTypeTranslate(card.offer.type);
-    cardElement.querySelector('.popup__text--capacity').textContent = `${card.offer.rooms} комнаты для ${card.offer.guests} гостей`;
-    cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${card.offer.checkin}, выезд до ${card.offer.checkout}`;
-    const features = cardElement.querySelector('.popup__features');
-    removeChildrens(features);
-    for (let index = 0; index < card.offer.features.length; index++) {
-      const li = document.createElement('li');
-      li.classList.add('popup__feature', `popup__feature--${card.offer.features[index]}`);
-      features.appendChild(li);
-    }
-    if (card.offer.description) {
-      cardElement.querySelector('.popup__description').textContent = card.offer.description;
-    } else {
-      cardElement.querySelector('.popup__description').remove();
-    }
-    const photos = cardElement.querySelector('.popup__photos');
-    removeChildrens(photos);
-    for (let index = 0; index < card.offer.photos.length; index++) {
-      const img = document.createElement('img');
-      img.classList.add('popup__photo');
-      img.width = 45;
-      img.height = 40;
-      img.alt = 'Фотография жилья';
-      img.src = card.offer.photos[index];
-      photos.appendChild(img);
-    }
-    cardElement.querySelector('.popup__avatar').src = card.author.avatar;
-    mapCanvas.appendChild(cardElement);
-  });
+const generateCard = ((card) => {
+  const cardElement = similarCardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = `${card.offer.price} ₽/ночь`;
+  cardElement.querySelector('.popup__type').textContent = getTypeTranslate(card.offer.type);
+  cardElement.querySelector('.popup__text--capacity').textContent = `${card.offer.rooms} комнаты для ${card.offer.guests} гостей`;
+  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${card.offer.checkin}, выезд до ${card.offer.checkout}`;
+  const features = cardElement.querySelector('.popup__features');
+  removeChildrens(features);
+  for (let index = 0; index < card.offer.features.length; index++) {
+    const li = document.createElement('li');
+    li.classList.add('popup__feature', `popup__feature--${card.offer.features[index]}`);
+    features.appendChild(li);
+  }
+  if (card.offer.description) {
+    cardElement.querySelector('.popup__description').textContent = card.offer.description;
+  } else {
+    cardElement.querySelector('.popup__description').remove();
+  }
+  const photos = cardElement.querySelector('.popup__photos');
+  removeChildrens(photos);
+  for (let index = 0; index < card.offer.photos.length; index++) {
+    const img = document.createElement('img');
+    img.classList.add('popup__photo');
+    img.width = 45;
+    img.height = 40;
+    img.alt = 'Фотография жилья';
+    img.src = card.offer.photos[index];
+    photos.appendChild(img);
+  }
+  cardElement.querySelector('.popup__avatar').src = card.author.avatar;
+  return cardElement;
 });
 
 export {
-  generateCards
+  generateCard
 };
